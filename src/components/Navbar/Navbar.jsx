c9d903f5a8156b9779111ccce8d3d165ad9566e6
Rahil Habibli <rahilah@code.edu.az>
2023-08-13 15:50:12 +0400

    signout logic added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,26 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "../.././Styles/Navbar/Navbar.module.css";
 import useUser from "../../customhooks/useUser";
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export function Navbar() {
   const { isAuthenticated, userId, email, userRole } = useUser();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    const token = localStorage.getItem("token");
+    axios
+      .post("https://localhost:7013/api/Accounts/Logout", null, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(() => {
+        localStorage.removeItem("token");
+        window.dispatchEvent(new Event("storage"));
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("An error occurred during logout:", error);
+      });
+  };
 
   useEffect(() => {}, [isAuthenticated]);
   return (
@@ -20,7 +37,7 @@ export function Navbar() {
       <div className={styles.navbarRightSide}>
         <div className={styles.navbarComponentsRight}>
           {isAuthenticated ? (
-            <Link className={styles.signIn} to="/signin">
+            <Link className={styles.signIn} onClick={handleLogout}>
               Sign out
             </Link>
           ) : (
