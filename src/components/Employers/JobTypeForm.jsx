773ef6563ec6db78ea53cbe518a7a6ac3f5acfa0
Rahil Habibli <rahilah@code.edu.az>
2023-08-18 15:22:34 +0400

    Job type form buttons added

--- src/components/Employers/JobTypeForm.jsx
+++ src/components/Employers/JobTypeForm.jsx
@@ -1,78 +1,107 @@
+import React, { useEffect, useState } from "react";
 import {
+  Box,
+  Button,
+  Flex,
   FormControl,
   FormLabel,
-  GridItem,
-  Heading,
-  Input,
-  Select,
+  Text,
 } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-
+import { FiPlus } from "react-icons/fi";
+import { FiMinus } from "react-icons/fi";
 export const JobTypeForm = ({ formik }) => {
-  const [locations, setLocations] = useState([]);
+  const [jobTypes, setJobTypes] = useState([]);
+  const [selectedJobTypes, setSelectedJobTypes] = useState([]);
+
   useEffect(() => {
-    const fetchData = async (url, setDataFunction) => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch("https://localhost:7013/api/JobTypes");
         const data = await response.json();
-        setDataFunction(data);
+        setJobTypes(data);
       } catch (error) {
-        console.error(`Error fetching data from ${url}:`, error);
+        console.error(`Error fetching data from API:`, error);
       }
     };
 
-    fetchData("https://localhost:7013/api/Locations/GetAll", setLocations);
+    fetchData();
   }, []);
+
+  const handleAddJobType = (jobType) => {
+    if (selectedJobTypes.find((jt) => jt.id === jobType.id)) return;
+    const newSelectedJobTypes = [...selectedJobTypes, jobType];
+    setSelectedJobTypes(newSelectedJobTypes);
+    formik.setFieldValue(
+      "jobTypeId",
+      newSelectedJobTypes.map((jt) => jt.id).join(",")
+    );
+  };
+
+  const handleRemoveJobType = (jobTypeId) => {
+    const newSelectedJobTypes = selectedJobTypes.filter(
+      (jt) => jt.id !== jobTypeId
+    );
+    setSelectedJobTypes(newSelectedJobTypes);
+    formik.setFieldValue(
+      "jobTypeId",
+      newSelectedJobTypes.map((jt) => jt.id).join(",")
+    );
+  };
+
   return (
     <>
-      <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
-        Job Vacancy
-      </Heading>
       <FormControl isRequired>
         <FormLabel
-          htmlFor="jobTitle"
           fontWeight="md"
           color="gray.700"
           _dark={{ color: "gray.50" }}
         >
-          Job Title
+          Job Types
         </FormLabel>
-        <Input
-          id="jobTitle"
-          name="jobTitle"
-          onChange={formik.handleChange}
-          value={formik.values.jobTitle}
-        />
-      </FormControl>
-      <FormControl as={GridItem} colSpan={[6, 3]} isRequired>
-        <FormLabel
-          htmlFor="country"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{ color: "gray.50" }}
-        >
-          Choose your Job location
-        </FormLabel>
-        <Select
-          id="country"
-          name="country"
-          autoComplete="country"
-          placeholder="Select option"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.country}
-        >
-          {locations.map((loc) => (
-            <option key={loc.id} value={loc.id}>
-              {loc.location}
-            </option>
+        <Flex wrap="wrap">
+          {jobTypes.map((jobType) => (
+            <Button
+              key={jobType.id}
+              color="blue.500"
+              borderColor="blue.500"
+              borderWidth="1px"
+              borderRadius="12px"
+              bg="white"
+              m={2}
+              _hover={{
+                color: "white",
+                bg: "blue.500",
+              }}
+              onClick={() => handleAddJobType(jobType)}
+            >
+              {jobType.typeName} <FiPlus />
+            </Button>
           ))}
-        </Select>
+        </Flex>
+        <Box borderWidth="1px" rounded="lg" p={3} mt={3}>
+          <Text fontWeight="bold">Selected Job Types:</Text>
+          <Flex wrap="wrap">
+            {selectedJobTypes.map((jobType) => (
+              <Flex key={jobType.id} align="center" m={2} width="200px">
+                <Button
+                  borderColor="blue.500"
+                  borderWidth="1px"
+                  borderRadius="12px"
+                  bg="white"
+                  color="blue.500"
+                  _hover={{
+                    color: "white",
+                    bg: "red.500",
+                    borderColor: "red.500",
+                  }}
+                  onClick={() => handleRemoveJobType(jobType.id)}
+                >
+                  {jobType.typeName}
+                </Button>
+              </Flex>
+            ))}
+          </Flex>
+        </Box>
       </FormControl>
     </>
   );
