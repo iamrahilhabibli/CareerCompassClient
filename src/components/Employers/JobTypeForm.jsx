6847380cc5a84def2b5ca4eb0abf843d328b0871
Rahil Habibli <rahilah@code.edu.az>
2023-08-18 18:08:57 +0400

    Schedules section added JobType

--- src/components/Employers/JobTypeForm.jsx
+++ src/components/Employers/JobTypeForm.jsx
@@ -4,21 +4,48 @@ import { FiPlus, FiCheck } from "react-icons/fi";
 
 export const JobTypeForm = ({ formik }) => {
   const [jobTypes, setJobTypes] = useState([]);
+  const [schedules, setSchedules] = useState([]);
+  const [selectedSchedules, setSelectedSchedules] = useState([]);
   const [selectedJobTypes, setSelectedJobTypes] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (url, setDataFunction) => {
       try {
-        const response = await fetch("https://localhost:7013/api/JobTypes");
+        const response = await fetch(url);
         const data = await response.json();
-        setJobTypes(data);
+        setDataFunction(data);
       } catch (error) {
-        console.error(`Error fetching data from API:`, error);
+        console.error(`Error fetching data from ${url}:`, error);
       }
     };
 
-    fetchData();
+    fetchData("https://localhost:7013/api/JobTypes", setJobTypes);
+    fetchData("https://localhost:7013/api/Schedules/GetAll", setSchedules);
   }, []);
+
+  const handleAddSchedule = (schedule) => {
+    if (selectedSchedules.find((s) => s.id === schedule.id)) {
+      handleRemoveSchedule(schedule.id);
+      return;
+    }
+    const newSelectedSchedules = [...selectedSchedules, schedule];
+    setSelectedSchedules(newSelectedSchedules);
+    formik.setFieldValue(
+      "scheduleId",
+      newSelectedSchedules.map((s) => s.id).join(",")
+    );
+  };
+
+  const handleRemoveSchedule = (scheduleId) => {
+    const newSelectedSchedules = selectedSchedules.filter(
+      (s) => s.id !== scheduleId
+    );
+    setSelectedSchedules(newSelectedSchedules);
+    formik.setFieldValue(
+      "scheduleId",
+      newSelectedSchedules.map((s) => s.id).join(",")
+    );
+  };
 
   const handleAddJobType = (jobType) => {
     if (selectedJobTypes.find((jt) => jt.id === jobType.id)) {
@@ -43,7 +70,6 @@ export const JobTypeForm = ({ formik }) => {
       newSelectedJobTypes.map((jt) => jt.id).join(",")
     );
   };
-
   return (
     <>
       <FormControl isRequired>
@@ -84,6 +110,48 @@ export const JobTypeForm = ({ formik }) => {
                 <FiPlus />
               )}{" "}
               {jobType.typeName}
+            </Button>
+          ))}
+        </Flex>
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel
+          fontWeight="md"
+          color="gray.700"
+          _dark={{ color: "gray.50" }}
+        >
+          Schedule
+        </FormLabel>
+        <Flex wrap="wrap">
+          {schedules.map((schedule) => (
+            <Button
+              key={schedule.id}
+              borderColor="blue.500"
+              borderWidth="1px"
+              borderRadius="12px"
+              bg={
+                selectedSchedules.find((s) => s.id === schedule.id)
+                  ? "blue.500"
+                  : "white"
+              }
+              color={
+                selectedSchedules.find((s) => s.id === schedule.id)
+                  ? "white"
+                  : "blue.500"
+              }
+              m={2}
+              _hover={{
+                color: "white",
+                bg: "blue.500",
+              }}
+              onClick={() => handleAddSchedule(schedule)}
+            >
+              {selectedSchedules.find((s) => s.id === schedule.id) ? (
+                <FiCheck />
+              ) : (
+                <FiPlus />
+              )}{" "}
+              {schedule.shiftName}{" "}
             </Button>
           ))}
         </Flex>
