d36e45e37786d96640a19753f516400396d3344d
Rahil Habibli <rahilah@code.edu.az>
2023-08-20 13:13:50 +0400

    Search Result Layout changed

--- src/components/VacancyDetailsSearch/SearchResultCards.jsx
+++ src/components/VacancyDetailsSearch/SearchResultCards.jsx
@@ -1,8 +1,10 @@
-import { Box, Spinner, Text } from "@chakra-ui/react";
+import { Box, Flex, Spinner, Text } from "@chakra-ui/react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useVacancies } from "../../services/getVacancies";
 import moment from "moment";
+import { useState } from "react";
 export function SearchResultCards({ searchResults }) {
+  const [selectedVacancy, setSelectedVacancy] = useState(null);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const jobTitle = queryParams.get("jobTitle");
@@ -29,49 +31,51 @@ export function SearchResultCards({ searchResults }) {
   if (isError) {
     navigate("/somethingwentwrong");
   }
-  console.log(vacancies);
-  return (
-    <Box display="flex" flexWrap="wrap" justifyContent="space-around">
-      {vacancies?.map((result) => (
-        <Box
-          key={result.companyId}
-          maxW="sm"
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          m={4}
-        >
-          <Box p="6">
-            <Box
-              mt="1"
-              fontWeight="semibold"
-              as="h4"
-              lineHeight="tight"
-              isTruncated
-            >
-              {result.jobTitle}
-            </Box>
-            <Text color="gray.500">Company ID: {result.companyName}</Text>
-            <Text>Location ID: {result.locationName}</Text>
-            <Text>Salary: ${result.salary}</Text>
-            <Text>Job Types: {result.jobTypeIds.join(", ")}</Text>
-            <Text>
-              Shifts and Schedules: {result.shiftAndScheduleIds.join(", ")}
-            </Text>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: `Description: ${result.description}`,
-              }}
-            />
 
-            <Text>
-              Company Link:{" "}
-              <a href={result.companyLink}>{result.companyLink}</a>
-            </Text>
-            <Text>Date Created: {moment(result.dateCreated).fromNow()}</Text>
+  return (
+    <Flex flexDirection={"column"} maxWidth={"40%"}>
+      <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+        {vacancies?.map((result) => (
+          <Box
+            key={result.id}
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            m={4}
+            onClick={() => setSelectedVacancy(result)}
+            cursor="pointer"
+          >
+            <Box p="6">
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated
+              >
+                {result.jobTitle}
+              </Box>
+              <Text color="gray.500">Company ID: {result.companyName}</Text>
+              <Text>Location ID: {result.locationName}</Text>
+              <Text>Salary: ${result.salary}</Text>
+              <Text>Job Types: {result.jobTypeIds.join(", ")}</Text>
+              <Text>
+                Shifts and Schedules: {result.shiftAndScheduleIds.join(", ")}
+              </Text>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `Description: ${result.description}`,
+                }}
+              />
+              <Text>
+                Company Link:{" "}
+                <a href={result.companyLink}>{result.companyLink}</a>
+              </Text>
+              <Text>Date Created: {moment(result.dateCreated).fromNow()}</Text>
+            </Box>
           </Box>
-        </Box>
-      ))}
-    </Box>
+        ))}
+      </Box>
+    </Flex>
   );
 }
