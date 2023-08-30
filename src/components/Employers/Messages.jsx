2723f4097701181feff7b2621186685a71e5b3d1
Rahil Habibli <rahilah@code.edu.az>
2023-08-30 14:08:00 +0400

    Messages ui approved applicants

--- src/components/Employers/Messages.jsx
+++ src/components/Employers/Messages.jsx
@@ -1,7 +1,53 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Flex,
+  Heading,
+  Spinner,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import React from "react";
 import inboxImg from "../../images/inbox.png";
+import axios from "axios";
+import { useQuery } from "react-query";
+import useUser from "../../customhooks/useUser";
 export function Messages() {
+  const toast = useToast();
+  const { userId } = useUser();
+
+  const fetchApprovedApplicants = async () => {
+    const { data } = await axios.get(
+      `https://localhost:7013/api/JobApplications/GetApprovedApplicants/${userId}`
+    );
+    return data;
+  };
+
+  const {
+    data: approvedApplicants,
+    isLoading,
+    isError,
+  } = useQuery(["approvedApplicants", userId], fetchApprovedApplicants, {
+    retry: 1,
+    refetchOnWindowFocus: false,
+    enabled: !!userId,
+  });
+
+  const openChatWithApplicant = (applicant) => {};
+
+  if (isLoading) return <Spinner />;
+  if (isError) {
+    toast({
+      title: "An error occurred.",
+      description: "Unable to load applicants.",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+      position: "top-right",
+    });
+    return;
+  }
+
   return (
     <Box
       rounded={"lg"}
@@ -30,12 +76,27 @@ export function Messages() {
       </Box>
       <Box my={4} />
 
-      <Box
-        borderWidth={"1px"}
-        rounded={"lg"}
-        bg={"white"}
-        shadow="1px 1px 3px rgba(0,0,0,0.3)"
-      ></Box>
+      <Box my={4}>
+        {approvedApplicants?.map((applicant) => (
+          <Flex
+            key={applicant.id}
+            p={3}
+            cursor="pointer"
+            borderWidth="1px"
+            borderRadius="md"
+            onClick={() => openChatWithApplicant(applicant)}
+          >
+            <Avatar
+              src={applicant.avatar || "https://via.placeholder.com/40"}
+              size="sm"
+            />
+            <Box ml={4}>
+              <Text fontWeight="bold">{`${applicant.firstName} ${applicant.lastName}`}</Text>
+              <Text fontSize="sm">Click to chat</Text>
+            </Box>
+          </Flex>
+        ))}
+      </Box>
     </Box>
   );
 }
