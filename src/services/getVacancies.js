6f550b6375ca2701813f73b2a2001b69dbe73b03
Rahil Habibli <rahilah@code.edu.az>
2023-09-06 17:19:50 +0400

    GetVacancies Pagination Start

--- src/services/getVacancies.js
+++ src/services/getVacancies.js
@@ -1,21 +1,18 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-const fetchVacancies = async (jobTitle, locationId) => {
+const fetchVacancies = async (jobTitle, locationId, page, pageSize) => {
   const url = `https://localhost:7013/api/Vacancies/GetFilteredSearch`;
-  const params = { jobTitle: jobTitle || null };
-  if (locationId) {
-    params.locationId = locationId;
-  }
+  const params = { jobTitle: jobTitle || null, locationId, page, pageSize };
 
   const response = await axios.get(url, { params });
   return response.data;
 };
 
-export const useVacancies = (jobTitle, locationId) => {
+export const useVacancies = (jobTitle, locationId, page = 1, pageSize = 6) => {
   return useQuery(
-    ["vacancies", jobTitle, locationId],
-    () => fetchVacancies(jobTitle, locationId),
+    ["vacancies", jobTitle, locationId, page],
+    () => fetchVacancies(jobTitle, locationId, page, pageSize),
     {
       enabled: !!jobTitle || !!locationId,
     }
